13c133ca516b9346611576366850faa155f6b6cd
meghanasrividya <meghanar19@gmail.com>
2022-10-10 15:58:48 +0100

    first commit

---
+++ env_test.py
@@ -0,0 +1,30 @@
+#Python Variable?
+# To store user data-hard code the data- any other data
+
+first_name="Meghana"# - this is String
+last_name = "Aenugu"
+DOB =99 #-Int
+travel=14.4 #-float
+salary = 40000 #-int
+gross_salary = "salary+ travel"
+
+# salary=50
+# travel = 3.5
+
+# print(first_name)
+print(first_name)
+print(type(travel))
+print(type(last_name))
+
+# interact with users by taking user data in - input()
+print("Good Morning, Please Enter your name")
+name = input() # took user input in the var called name and printed
+print("Hello dear!")
+print(name)
+
+# Get user name first_name and last_name
+# display the names in the line
+# User DOB
+# course name
+# UK_resident
+# add it to your README.md once completed with notes
---
+++ python_variables.py
@@ -0,0 +1,14 @@
+# Get user first_name and last_name
+first_name=input("Enter your first_name") # Takes the input from the user and stores in the variable first_name
+last_name=input("Enter your last_name") # Takes the input from the user and stores in the variable last_name
+
+# display the names in the line
+print(first_name + last_name )
+dob=input("Please , enter your DOB:") # Takes the input dob from user
+print(dob)
+# course name
+course_name=input("Please, Enter your course name :")
+print(course_name)
+# UK_resident
+uk_resident =input ("Are you a resident of UK ?")
+print(uk_resident)
