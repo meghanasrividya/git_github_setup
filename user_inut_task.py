b24b025b1c1675b9188936668c885345a7ad86df
meghanasrividya <meghanar19@gmail.com>
2022-10-11 14:32:38 +0100

    added programs

---
+++ user_inut_task.py
@@ -0,0 +1,27 @@
+ #Get user first_name and last_name
+first_name=input("Enter your first_name") # Takes the input from the user and stores in the variable first_name
+last_name=input("Enter your last_name") # Takes the input from the user and stores in the variable last_name
+
+# display the names in the line
+print(first_name + last_name )
+dob=input("Please , enter your DOB:") # Takes the input dob from user
+
+#salary
+salary=input("Please, enter your salary")
+# course name
+course_name=input("Please, Enter your course name :")
+
+# UK_resident
+uk_resident =input ("Are you a resident of UK ?")
+
+address = input("Could you please enter the address")
+hobbies= input("Could you please enter your hobbies")
+
+print( " Hello " + first_name +" "+ last_name+"Your details are:")
+print("\n Full name: "+first_name +" "+ last_name)
+print("\n DOB: "+dob)
+print("\n salary : "+ str(salary))
+print("\n Course name: "+course_name)
+print("\n UK resident :"+ uk_resident)
+print("\n address: "+ address)
+print("\n hobbies: " +hobbies)
